6a7fd904e2be6ac24bc625c758b15ac3f3def0f7
DiegoNicolasPenaRincon <146769438+DiegoNicolasPenaRincon@users.noreply.github.com>
2025-09-05 11:34:45 -0500

    Se agrego la funcionalidad para marcar un sitio en especifico del mapa

--- src/app/componentes/registro-inmueble/registro-inmueble.component.ts
+++ src/app/componentes/registro-inmueble/registro-inmueble.component.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MapaService} from '../../mapa.service';
+import {FormBuilder, FormGroup, isFormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {UsersService} from '../../servicios/users.service';
+import {RegistroInmuebleRequest} from '../../dto/registro-inmueble-request';
 
 @Component({
   selector: 'app-registro-inmueble',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './registro-inmueble.component.html',
   styleUrl: './registro-inmueble.component.css'
 })
-export class RegistroInmuebleComponent {
+export class RegistroInmuebleComponent implements OnInit{
+  registroInmuebleForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private mapaService: MapaService) {
+    this.crearFormularioTexto();
+  }
+
+  //Revisar si funciona
+  ngOnInit(): void {
+    this.mapaService.crearMapa();
+
+
+    this.mapaService['mapa'].on('click', (event: any) => {
+      const { lng, lat } = event.lngLat;
+      this.mapaService.agregarMarcador();
+
+      this.registroInmuebleForm.get('ubicacion')?.setValue({ latitud: lat, longitud: lng });
+    });
+  }
+
+  //Modificar despues xd
+  private crearFormularioTexto() {
+    this.registroInmuebleForm = this.formBuilder.group({
+      tipoNegocio: ['', [Validators.required]],
+      tipoInmueble: ['', [Validators.required]],
+      precio: ['', [Validators.required]],
+      Estrato: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      terminosycondiciones: [false, [Validators.requiredTrue]],
+      recibirPromociones: [false, [Validators.requiredTrue]],
+      password: ['', [Validators.required, Validators.maxLength(50), Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required, Validators.maxLength(50), Validators.minLength(8)]],
+      ubicacion: this.formBuilder.group({
+        latitud: [''],
+        longitud: ['']
+      })
+    });
+  }
+
+  onSubmit() {
+
+  }
+
 
 }
+
+
+
