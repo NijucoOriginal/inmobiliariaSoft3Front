8caf65ecf77fb6217ca63735991d98c6ecc2152b
DiegoNicolasPenaRincon <146769438+DiegoNicolasPenaRincon@users.noreply.github.com>
2025-09-21 13:13:41 -0500

    Update users.service.ts

--- src/app/servicios/users.service.ts
+++ src/app/servicios/users.service.ts
@@ -8,7 +8,7 @@ import {UserRegistrationRequest} from '../dto/user-registration-request';
   providedIn: 'root'
 })
 export class UsersService {
-  private url = "https://backend-eden.onrender.com/api/usuarios";
+  private url = "https://inmobiliariasoft3back2-0.onrender.com/api/usuarios";
   constructor(private http: HttpClient) {}
 
   public registrar(user: UserRegistrationRequest): Observable<UserResponse> {
