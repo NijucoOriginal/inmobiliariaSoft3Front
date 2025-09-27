4b03b8b3dd9136ec1b11268ab814598cc1ddb14e
Astolfo34 <jsebasdroid31@gmail.com>
2025-09-27 11:50:42 -0500

    fRONT ajustado --- migracion de collaborador Astolfo a aAlzateSebastian
    estado: funciona-por verificar nuevas vistas
    descripcion: se crearon 2 tipos de pagina inicio , para invitados y para usuarios de la app.
    
    * se definio una clase de servicios para extraer el rol y permitir mostar la vista correspondiente al rol (cliente, admin)-- esto usando ngSwitch en el html de inicio.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,23 +8,72 @@ import {UnauthorizedComponent} from './componentes/unauthorized/unauthorized.com
 import {RegistroInmuebleComponent} from './componentes/registro-inmueble/registro-inmueble.component';
 import {RecuperarContraseniaComponent} from './componentes/recuperar-contrasenia/recuperar-contrasenia.component';
 import {ActivarComponent} from './componentes/activar-cuenta/activar.component';
+import { InicioDefaultComponent } from './componentes/inicio-default/inicio-default.component';
 
 import {authGuard} from './guards/auth.guard';
 import {rolesGuard} from './guards/roles.guard';
 
 
 export const routes: Routes = [
-  { path: '',
-    component: InicioComponent
+  {
+    path: '',
+    component: InicioDefaultComponent
   },
-  { path: 'login', component: LoginComponent, canActivate: [authGuard] },
-  { path: 'registro', component: RegistroComponent, canActivate: [authGuard]  },
-  { path: 'recuperar', component: RecuperarContraseniaComponent, canActivate: [authGuard] },
-  { path: 'activar', component: ActivarComponent, canActivate: [authGuard] },
-  { path: 'ventanaUsuario', component: VentanaUsuarioComponent, canActivate: [rolesGuard], data: { expectedRoles: ["USUARIO"] } },
-  { path: 'ventanaAgente', component: VentanaAgenteComponent, canActivate: [rolesGuard], data: { expectedRoles: ["AGENTE"] } },
-  { path: 'ventanaAsesor', component: VentanaAgenteComponent, canActivate: [rolesGuard], data: { expectedRoles: ["ASESOR"] } },
-  {path: 'registroInmueble', component: RegistroInmuebleComponent, canActivate: [authGuard]},
-  { path: 'unauthorized', component: UnauthorizedComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '/' },
+  {
+    path: 'inicio',
+    component: InicioComponent,
+    canActivate: [authGuard, rolesGuard],
+    data: { expectedRoles: ["CLIENTE", "ASESOR"] }
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'registro',
+    component: RegistroComponent
+  },
+  // Rutas protegidas para usuarios logueados
+  {
+    path: 'recuperar',
+    component: RecuperarContraseniaComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'activar',
+    component: ActivarComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'ventanaUsuario',
+    component: VentanaUsuarioComponent,
+    canActivate: [rolesGuard],
+    data: { expectedRoles: ["USUARIO"] }
+  },
+  {
+    path: 'ventanaAgente',
+    component: VentanaAgenteComponent,
+    canActivate: [rolesGuard],
+    data: { expectedRoles: ["AGENTE"] }
+  },
+  {
+    path: 'ventanaAsesor',
+    component: VentanaAgenteComponent,
+    canActivate: [rolesGuard],
+    data: { expectedRoles: ["ASESOR"] }
+  },
+  {
+    path: 'registroInmueble',
+    component: RegistroInmuebleComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'unauthorized',
+    component: UnauthorizedComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/inicio',
+    pathMatch: 'full'
+  },
 ];
