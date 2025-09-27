51b132c6ee9f06ca0f4b8a7bf318a55ee051036d
DiegoNicolasPenaRincon <146769438+DiegoNicolasPenaRincon@users.noreply.github.com>
2025-09-27 16:47:08 -0500

    commit falso para poder redesplegar el proyecto en netfly

--- src/app/servicios/users.service.ts
+++ src/app/servicios/users.service.ts
@@ -9,7 +9,7 @@ import {UserRegistrationRequest} from '../dto/user-registration-request';
 })
 export class UsersService {
   private url = "https://inmobiliariasoft3back2-0.onrender.com/api/usuarios";
-  //private url='http://localhost:8080/api/usuarios' commit prueba;
+  //private url='http://localhost:8080/api/usuarios' commit prueba 2.0;
   constructor(private http: HttpClient) {}
 
   public registrar(user: UserRegistrationRequest): Observable<UserResponse> {
