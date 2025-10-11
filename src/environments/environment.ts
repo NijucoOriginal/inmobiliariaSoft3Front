bc9c4bb9945c069c8811c6b70cb0e06490a2e6f0
Astolfo34 <jsebasdroid31@gmail.com>
2025-10-10 19:11:47 -0500

    configuracion de la variable de entorno global backend url para elegir el backend consumido por el front---desplegado en este caso

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  //backendUrl: 'https://backend-eden.onrender.com', // Dirección del backend para desarrollo
+  backendUrl: 'https://backend-eden.onrender.com', // Dirección del backend para desarrollo
   // Direccion del backend local
-  backendUrl: 'http://localhost:8080'
+  //backendUrl: 'http://localhost:8080'
 };
 
