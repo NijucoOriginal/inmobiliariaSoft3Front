4226c8d58bb776d583529b48e72e5e4a623719e5
DiegoNicolasPenaRincon <146769438+DiegoNicolasPenaRincon@users.noreply.github.com>
2025-10-20 09:26:30 -0500

    hola

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   production: true,
   backendUrl: 'https://backend-eden.onrender.com', // Dirección del backend para desarrollo
   // Direccion del backend local
-  //backendUrl: 'http://localhost:8080'
+  //backendUrl: 'http://localhost:8080' commit falso
 }
 
